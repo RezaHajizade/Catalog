2442100c90d8cfbc49cf2efda4c8a479cb79f97c
reza hajizade <rezahajizadedev@gmail.com>
2025-01-15 12:36:48 +0330

    Add item apis

---
+++ Catalog/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Catalog.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToKebabCase(this string input)
+        {
+            string sanitizedInput = Regex.Replace(input, @"[^a-zA-Z0-9\s-]", "");
+
+            string kebab = Regex.Replace(sanitizedInput.Trim(), @"\s+", "-");
+
+            return kebab.ToLower();
+        }
+    }
+}
